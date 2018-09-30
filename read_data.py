b75dbce8788f570c19c029cc2a05ea5243dbceaa
Patrick Falvey <patrickjafalvey@gmail.com>
2018-09-30 17:18:09 -0400

    add mapping

--- read_data.py
+++ read_data.py
@@ -2,16 +2,34 @@
 
 import pandas
 from pandas import Series, DataFrame
+from datetime import date
 
 def main():
     filename = "2016-capitalbikeshare-tripdata/2016Q1-capitalbikeshare-tripdata.csv"
     data = read_data(filename)
+    data = map_data(data)
+    print(data)
     return data
 
 def read_data(name_file):
     dataframe = pandas.read_csv(name_file)
     dataframe.drop(["Member type", "Duration", "Bike number", "Start station", "End station"], axis=1, inplace=True)
     return dataframe
+
+def map_data(data):
+    Days = []
+    count = 4
+    month = 1
+    day = 1
+    #0 is monday, 6 is sunday
+    #1/1/2016 is a friday, so start count at 4
+    for i in data["Start date"]:
+        newi = i.split(" ")
+        newerI = newi[0].split("-")
+        Days.append(date(int(newerI[0]), int(newerI[1]), int(newerI[2])).weekday())
+    data["Day of week"] = Days
+    return data
+
 
 if __name__ == "__main__":
     main()
